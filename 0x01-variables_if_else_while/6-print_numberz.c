4553aac2b8bc9c470c29d751f7ff4e6564869c62
hendrixxD <maticsjnr@gmail.com>
2022-04-08 11:19:06 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints reversed alphabet.
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -9,11 +9,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 122; i > 96; i--)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
 	return (0);
-}
-
+}}

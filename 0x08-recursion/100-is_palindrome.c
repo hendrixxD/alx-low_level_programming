59bcde89cd786b4e4460daa0f6f105f18bedf9f6
hendrixxD <maticsjnr@gmail.com>
2022-04-28 16:00:41 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,48 @@
+
 #include "main.h"
+
 /**
- * is_palindrome-  Returns the natural square root of a number.
- * @s: int type
- * Return: char type
- **/
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int is_palindrome(char *s)
 {
-int first, last;
+	int l;
 
-first = 0;
-last = _strlen_recursion(s) - 1;
-return (palindrome(s, first, last));
-}
-
-/**
- * palindrome - Entry point
- * Desc: palindrome
- * @s: char type
- * @first: int type
- * @last: int type
- * Return: Recursion
- **/
-int palindrome(char *s, int first, int last)
-{
-if (first > last)
-{
-return (1);
-}
-else if (s[first] == s[last])
-{
-return (palindrome(s, first + 1, last - 1));
-}
-else
-return (0);
-}
-
-/**
- * _strlen_recursion - Entry point
- * Desc: _strlen_recursion
- * @s: char type
- * Return: Function that returns the length of a string.
- **/
-int _strlen_recursion(char *s)
-{
-if (*s != '\0')
-{
-return (1 + _strlen_recursion(s + 1));
-}
-return (0);
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }

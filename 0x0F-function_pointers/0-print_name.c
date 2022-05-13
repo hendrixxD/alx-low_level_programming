c0afc7bcb7f1236db648f6a18061a8c4ec2e3f8e
hendrixxD <maticsjnr@gmail.com>
2022-05-13 06:50:09 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,14 @@
-/*
- * File: 0-print_name.c
- * Author: Yovo Koffi Vianney
- */
-
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to be printed
- * @f: callback function that prints name
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!f || !name)
+	if (name == NULL || f == NULL)
 		return;
-	(*f)(name);
+
+	f(name);
 }
